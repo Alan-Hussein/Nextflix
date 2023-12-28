7b19505feed94f0da3873b7730bc2d4ec90cc472
Alan Hussein <alanhussein116@gmail.com>
2023-12-28 22:51:32 +0100

    MovieCard Styles

--- nextflix/src/Components/MovieCard/MovieCard.tsx
+++ nextflix/src/Components/MovieCard/MovieCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-
+import Styles from './MovieCard.module.css'
 interface MovieCardProps {
   title: string;
   posterPath: string;
@@ -8,7 +8,7 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ title, posterPath }) => {
   return (
-    <div>
+    <div className={Styles.movieCard}>
       <h3>{title}</h3>
       <Image
         src={`https://image.tmdb.org/t/p/w500/${posterPath}`}
--- nextflix/src/Components/Popular/Popular.tsx
+++ nextflix/src/Components/Popular/Popular.tsx
@@ -1,10 +1,13 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { fetchPopularMovies } from '../../Utils/useFetch';
 import MovieCard from '../MovieCard/MovieCard';
+import styles from '../MovieCard/MovieCard.module.css';
 
 const Popular: React.FC = () => {
   const [movies, setMovies] = useState<{ title: string; poster_path: string }[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const touchStartX = useRef<number | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,18 +18,81 @@ const Popular: React.FC = () => {
     fetchData();
   }, []);
 
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (touchStartX.current === null) {
+      return;
+    }
+
+    const deltaX = touchStartX.current - e.touches[0].clientX;
+    touchStartX.current = e.touches[0].clientX;
+
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += deltaX;
+    }
+  };
+
+  const handleTouchEnd = () => {
+    touchStartX.current = null;
+  };
+
+  const handleButtonClick = (direction: 'left' | 'right') => {
+    const container = containerRef.current;
+    const cardWidth = 340; // Adjust the card width based on styles
+    const scrollAmount = direction === 'left' ? -cardWidth : cardWidth;
+
+    if (container) {
+      container.scrollLeft += scrollAmount;
+    }
+  };
+
+  const handleWheel = (e: React.WheelEvent<HTMLDivElement>) => {
+    e.preventDefault(); // Prevent the default behavior
+  
+    const container = containerRef.current;
+    const cardWidth = 340; // Adjust the card width based on your styles
+  
+    if (container) {
+      if (e.deltaY > 0) {
+        container.scrollLeft += cardWidth; // Scroll right
+      } else {
+        container.scrollLeft -= cardWidth; // Scroll left
+      }
+    }
+  
+    // Stop the propagation of the wheel event to prevent it from affecting parent elements
+    e.stopPropagation();
+  };
+  
+
   return (
     <div>
       <h1>Most Popular Movies</h1>
-      <ul>
+      <div className={styles['navigation-buttons']}>
+        <button onClick={() => handleButtonClick('left')}>{'<'}</button>
+        <button onClick={() => handleButtonClick('right')}>{'>'}</button>
+      </div>
+      <div
+        id="movie-card-container"
+        className={styles['movie-card-container']}
+        ref={containerRef}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+        onWheel={handleWheel}
+      >
         {movies.map((movie, index) => (
-          <li key={index}>
-            <MovieCard title={movie.title} posterPath={movie.poster_path} />
-          </li>
+          <MovieCard key={index} title={movie.title} posterPath={movie.poster_path} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
 
 export default Popular;
+
+
+
