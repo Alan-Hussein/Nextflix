362e79a3a7976a5a3cacefcba0471038f27dde3e
Alan Hussein <alanhussein116@gmail.com>
2024-06-08 17:56:11 +0200

    change website's title

--- nextflix/src/Components/layouts/RootLayout.tsx
+++ nextflix/src/Components/layouts/RootLayout.tsx
@@ -14,7 +14,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Create Next App</title>
+        <title>Nextflix</title>
       </Head>
       {children}
     </>
