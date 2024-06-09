cc737936a60178e36d7277d57fd7f6cdc682eeeb
Alan Hussein <alanhussein116@gmail.com>
2024-06-09 21:09:33 +0200

    Update theme

--- nextflix/src/Components/HomePage/HomePage.tsx
+++ nextflix/src/Components/HomePage/HomePage.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
-import styles from './HomePage.module.css'
+import styles from './HomePage.module.css';
+
 const HomePage = () => {
   return (
-    <div className={styles.homePage}>
-      <Image src="/images/home10.jpg" alt="HomePage" width={5000} height={500}  layout="responsive"
-/>
+    <div className={styles.container}>
+      <div className={styles.triangle}></div>
+      <div className={styles.overlay}>
+        <div className={styles.logo}>
+          <h1>Nextflix</h1>
+        </div>
+        <div className={styles.content}>
+          <h2>SEE WHAT’S <span className={styles.next}>NEXT.</span></h2>
+          <p>Watch anywhere. Cancel anytime.</p>
+          <button className={styles.joinButton}>Join free for a month</button>
+          <button className={styles.signInButton}>Sign In</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
